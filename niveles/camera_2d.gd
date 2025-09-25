cc3d5ae6ea7f46d3db1b2f34b7aa94930c11bcf7
David Navarro <navarro281205@gmail.com>
2025-09-24 22:12:36 -0500

    correccion mapa

--- niveles/camera_2d.gd
+++
@@ -1,8 +0,0 @@
-extends Camera2D
-
-@export var objet_seguir :CharacterBody2D
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position = objet_seguir.position
-	pass
